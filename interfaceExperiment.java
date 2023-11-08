f83bba513acdbeeeb75c12c5975ff9309b4e67e4
NdakizeYvan <149577126+NdakizeYvan@users.noreply.github.com>
2023-11-08 20:11:58 +0200

    Add files via upload

---
+++ interfaceExperiment.java
@@ -0,0 +1,38 @@
+class yearone implements End{
+    public void print(){
+      int marks=80;
+      System.out.println("student passed year 1 with: "+marks+" marks");
+    }
+} 
+ interface End {
+     void print(); 
+}
+class yeartwo extends yearone{
+  
+    int score=76;
+    void result(){
+    System.out.println("student passed year 2");
+  }
+}
+class yearthree extends yeartwo {
+   int total= 82;
+  void showing(){
+    System.out.println("student passed year 3");
+  }
+}
+//class D extends C{
+
+//}
+
+public class interfaceExperiment {
+    public static void main(String[] args) {
+        yeartwo b = new yeartwo();
+        yearthree c = new yearthree();
+        End e = new yearone();
+
+        System.out.println(b.score);
+        System.out.println(c.total);
+        e.print();
+    }
+    
+}
