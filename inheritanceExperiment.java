dcb19a494da320b5c37de7b37579bb2124d5d2b4
NdakizeYvan <149577126+NdakizeYvan@users.noreply.github.com>
2023-11-08 22:16:04 +0200

    changes..

--- inheritanceExperiment.java
+++ inheritanceExperiment.java
@@ -10,16 +10,38 @@ class country implements world{
         System.out.println("Rwanda");
     }
 }
-// declaring class "ranking" that includes fifa world ranking which is a child class of "country"
+// declaring class "ranking" that includes fifa world ranking number which is a child class of "country"
 class ranking extends country{
+    public void display(){
     int rank =140;
+    System.out.println(rank);
+}
   }
+//declaring class "points" which includes points scored
+class points extends ranking{
+    public void print(){
+    double total = 1087.03;
+    System.out.println(total);
+}
+}
+//declaring class"difference" which includes difference between previous and current points
+class difference extends points{
+    public void write(){
+    double diff=0;
+    System.out.println(diff);
+}
+}
 
 public class inheritanceExperiment {
     public static void main(String[] args) {
-        ranking R= new ranking();
-        world w =new country();
-       System.out.println(R.rank);// to display its fifa ranking
+        ranking R= new ranking(); //object of class ranking
+        points P = new points(); //object of class points
+        difference D = new difference(); //objects of class difference
+        world w =new country(); //
+
+       R.display();// to display its fifa ranking
         w.show();//to display the country name
+       P.print();// to display current points
+       D.write();//to display the difference
     }
 }
